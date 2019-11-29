456302e2ef4dfc9bf12314643f6c61f9b24d0445
efhiii <efhiii2000@gmail.com>
2019-11-28 17:56:08 -0800

    Scaling title

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Shapush
 A very unique Sokoban-like puzzle game
+
 Play at http://efhiii.github.io/shapush
--- game.js
+++ game.js
@@ -1,5 +1,5 @@
 const c = document.getElementById("canvas");
-const ctx = c.getContext("2d");
+const ctx = c.getContext("2d", { alpha: false });
 c.style.backgroundColor="red";
 c.width=window.innerWidth;
 c.height=window.innerHeight;
@@ -17,17 +17,48 @@ return true;
   return mq(query);
 }
 
-let w=c.width,h=c.height,mobile=isMobile();
+let w=c.width,h=c.height,mobile=isMobile(),ARType=1;
+let min=w>h?h:w;
+const title=new Image();
+title.src = 'assets/title.png';
 
+function getARType(AR){
+  if(AR<0.8){ARType=1;return;}
+  if(AR>1.2){ARType=3;return;}
+  ARType=2;
+}
+getARType();
 
-function drawCanvas(){
-  ctx.fillStyle="#323232";
-  if(mobile){
-    ctx.fillStyle="#f00";
+function s0(){
+  let tx=0;
+  let ty=0;
+  if(w>h){
+    tx=(w-min)/2;
   }
+  else{
+    ty=(h-min)/2;
+  }
+  ctx.drawImage(title,tx,ty,min,min);
+}
+function s1(){
+
+}
+
+scene=0;
+scenes=[s0,s1];
+function drawCanvas(){
+  ctx.fillStyle="#242729";
   ctx.fillRect(0,0,w,h);
+  /*
+  target ARs:
+  16:9
+  1:1
+  9:16
+  */
+  scenes[scene]();
   window.requestAnimationFrame(drawCanvas);
 }
+
 window.requestAnimationFrame(drawCanvas);
 
 window.onresize = ()=>{
@@ -35,6 +66,8 @@ window.onresize = ()=>{
   c.height=window.innerHeight;
   w=c.width;
   h=c.height;
+  min=w>h?h:w;
+  getARType();
 }
 
 document.ontouchmove = (event)=>{
